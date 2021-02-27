db2bd70305a543ff14bc3c78d14c54594aa35e0b
SureshK <sureshkrip@gmail.com>
2021-02-27 16:00:52 -0500

    app.py changed

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return "Hello World"
+  #  return render_template('index.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
