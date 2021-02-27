2b0c335f6b4dca40149427ddf48b452bd99ff963
SureshK <sureshkrip@gmail.com>
2021-02-27 16:15:12 -0500

    requirements changed

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return "Hello World"
-  #  return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
